2e4c45119eabc75e66ff482d25598e69c0ebe014
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-08-01 17:15:56 -0300

    Feat: Listando imagens com o FileManager.

--- project1/project1/ViewController.swift
+++ project1/project1/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default // atribui o valor retornado pelo gerenciador
+        let path = Bundle.main.resourcePath! // Define o caminho do recurso do nosso pacote
+        let items = try! fm.contentsOfDirectory(atPath: path) // Definir o conteúdo do diretório em um caminho
+        
+        for item in items { // um loop percorrendo todos os itens encontrados no diretório
+            if item.hasPrefix("nssl") { // Verifique se tem o prefixo nssl
+                pictures.append(item)
+            }
+        }
+        
+        print(pictures)
     }
 
 
