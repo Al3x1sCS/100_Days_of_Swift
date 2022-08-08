aae7ec67f48874551a8a763076428f05083bc7bb
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-08-08 12:33:12 -0300

    Feat: Challenge 1 e 2 - Projeto 4.

--- project4/project4/ViewController.swift
+++ project4/project4/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, WKNavigationDelegate {
+    
     var webView: WKWebView!
     var progressView: UIProgressView!
     var websites = ["apple.com", "hackingwithswift.com"]
@@ -26,12 +27,14 @@ class ViewController: UIViewController, WKNavigationDelegate {
         
         let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
         let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: webView, action: #selector(webView.reload))
+        let goBack = UIBarButtonItem(title: "Back", style: .plain, target: webView, action: #selector(webView.goBack))
+        let goForward = UIBarButtonItem(title: "Forward", style: .plain, target: webView, action: #selector(webView.goForward))
         
         progressView = UIProgressView(progressViewStyle: .default)
         progressView.sizeToFit()
         let progressButton = UIBarButtonItem(customView: progressView)
         
-        toolbarItems = [progressButton, spacer, refresh]
+        toolbarItems = [progressButton, spacer, goBack, goForward, refresh]
         navigationController?.isToolbarHidden = false
         
         webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
@@ -39,6 +42,12 @@ class ViewController: UIViewController, WKNavigationDelegate {
         let url = URL(string: "https://" + websites[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressView.progress = Float(webView.estimatedProgress)
+        }
     }
     
     func openPage(action: UIAlertAction) {
@@ -62,17 +71,22 @@ class ViewController: UIViewController, WKNavigationDelegate {
                 if host.contains(website) {
                     return decisionHandler(.allow)
                 }
+
             }
+
+        }
+        let urlString = url?.absoluteString ?? "Unknown"
+        
+        if urlString != "about:blank" {
+            let ac = UIAlertController(title: "NÃO AUTORIZADO!", message: "Website \"\(urlString)\" não faz parte de sites autorizados", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(ac, animated: true)
         }
         
         decisionHandler(.cancel)
     }
+
     
-    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == "estimatedProgress" {
-            progressView.progress = Float(webView.estimatedProgress)
-        }
-    }
     
     @objc func openTapped() {
         let ac = UIAlertController(title: "Abrindo pagina...", message: nil, preferredStyle: .actionSheet)
@@ -84,6 +98,15 @@ class ViewController: UIViewController, WKNavigationDelegate {
         ac.addAction(UIAlertAction(title: "Cancelar", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(ac, animated: true)
+    }
+    
+    @objc func goBackTapped(){
+        webView.goBack()
+    }
+    
+    
+    @objc func goForwardTapped(){
+        webView.goForward()
     }
 
 }
