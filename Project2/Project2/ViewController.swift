794f5c4b694a0e9cdb7f5221cc273c775dbb15f4
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-08-10 15:27:16 -0300

    Feat: Challenges - Projeto 2.

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score = 0
     var correctAnswer = 0
+    var currentQuestion = 0
+    
+    let maxQuestion = 10
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,9 +33,12 @@ class ViewController: UIViewController {
         button3.layer.borderColor = UIColor.systemPurple.cgColor
         
         askQuestion()
+        
     }
     
-    func askQuestion() {
+    func askQuestion(action: UIAlertAction! = nil) {
+        currentQuestion += 1
+        
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
         
@@ -41,6 +47,58 @@ class ViewController: UIViewController {
         button3.setImage(UIImage(named: countries[2]), for: .normal)
         
         title = countries[correctAnswer].uppercased()
+        
+        if currentQuestion > maxQuestion {
+            showResult()
+            return
+        }
+        
+        updateTitle()
+    }
+    
+    func updateTitle() {
+        title = "\(countries[correctAnswer].uppercased()) | Pontos: \(score) | \(currentQuestion) de \(maxQuestion)"
+    }
+    
+    func showResult() {
+        let ac = UIAlertController(title: "Fim do jogo", message: "Questões: \(maxQuestion)\nPontuação Final: \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Jogar de novo", style: .default, handler: askQuestion))
+
+        score = 0
+        correctAnswer = 0
+        currentQuestion = 0
+
+        present(ac, animated: true)
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        var message: String
+
+        if sender.tag == correctAnswer {
+            title = "Correto"
+            score += 1
+            message = "Pontuação: \(score)."
+        }
+        else {
+            title = "Errado"
+            score -= 1
+            
+            message = """
+                Sua escolha: \(countries[sender.tag].uppercased())
+                A bandeira da \(countries[correctAnswer].uppercased()) é a: #\(correctAnswer + 1)
+                Pontuação: \(score)
+                """
+        }
+        
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        
+        ac.addAction(UIAlertAction(title: "Continuar", style: .default, handler: askQuestion))
+        
+        
+        updateTitle()
+        
+        present(ac, animated: true)
     }
     
 }
