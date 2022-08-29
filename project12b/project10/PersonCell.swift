08bb91b4021b6960832797ab313bcbad2d9dc7cc
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-08-29 12:37:23 -0300

    feat: Iniciando projeto 12a e 12b.

---
+++ project12b/project10/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  project10
+//
+//  Created by user on 24/08/22.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
