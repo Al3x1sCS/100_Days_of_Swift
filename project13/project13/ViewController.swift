23b4a3652199500abfb80f215632ab94bc4158da
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-09-02 15:52:12 -0300

    feat: Importando uma imagem.

--- project13/project13/ViewController.swift
+++ project13/project13/ViewController.swift
@@ -7,14 +7,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var intensity: UISlider!
     
+    var currentImage: UIImage!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        title = "YACIFP"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
     }
 
     @IBAction func changeFilter(_ sender: Any) {
@@ -24,6 +30,21 @@ class ViewController: UIViewController {
     }
     
     @IBAction func intensityChanged(_ sender: Any) {
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        dismiss(animated: true)
+
+        currentImage = image
     }
 }
 
