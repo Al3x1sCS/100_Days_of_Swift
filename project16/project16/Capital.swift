43b615ebc92bc0bfc4480f049ab16f53482573d1
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-09-08 16:34:15 -0300

    feat: Conhecendo MapKit.

---
+++ project16/project16/Capital.swift
@@ -0,0 +1,20 @@
+//
+//  Capital.swift
+//  project16
+//
+//  Created by user on 08/09/22.
+//
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
