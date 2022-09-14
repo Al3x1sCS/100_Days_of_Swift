5c528313f1347b5ad7f1ac6424797ff97f489c78
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-09-14 14:31:24 -0300

    feat: Iniciando projeto 18 e 19.

---
+++ project18/project18/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  project18
+//
+//  Created by user on 14/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+        
+    }
+
+}
+
