eb784bfde44769d4391a21ab23c4e0fb0bfad334
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-09-20 11:21:55 -0300

    feat: Trabalhando com strings no Swift.

--- project24/project24.playground/Contents.swift
+++ project24/project24.playground/Contents.swift
@@ -15,3 +15,51 @@ extension String {
 }
 
 let letter2 = name[3]
+
+let password = "123456"
+
+password.hasPrefix("123")
+password.hasSuffix("456")
+
+extension String {
+    func deletingPrefix(_ prefix: String) -> String {
+        guard self.hasPrefix(prefix) else { return self }
+        return String(self.dropFirst(prefix.count))
+    }
+    
+    func deletingSuffix(_ suffix: String) -> String {
+        guard self.hasSuffix(suffix) else { return self }
+        return String(self.dropLast(suffix.count))
+    }
+}
+
+let weather = "Vai chover de leve, espero."
+print(weather.capitalized)
+
+extension String {
+    var capitalizedFirst: String {
+        guard let firstLetter = self.first else { return "" }
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+}
+
+
+let input = "Swift é que nem Objective-C mas sem o C"
+input.contains("Swift")
+
+let languages = ["Python", "Ruby", "Swift"]
+languages.contains("Swift")
+
+extension String {
+    func containsAny(of array: [String]) -> Bool {
+        for item in array {
+            if self.contains(item) {
+                return true
+            }
+        }
+
+        return false
+    }
+}
+
+input.containsAny(of: languages)
