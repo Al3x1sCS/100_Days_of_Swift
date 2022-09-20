ff78da52a96e3e363494f4af57c0b3dc468304ad
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-09-20 10:42:41 -0300

    feat: Strings não são arrays - Projeto 24.

---
+++ project24/project24.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var name = "Alexis"
+
+for letter in name {
+    print("Me de um \(letter)!")
+}
+
+let letter = name[name.index(name.startIndex, offsetBy: 3)]
+
+extension String {
+    subscript(i: Int) -> String {
+        return String(self[index(startIndex, offsetBy: i)])
+    }
+}
+
+let letter2 = name[3]
