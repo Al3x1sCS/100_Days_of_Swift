fc3ebd7d488e542b11d70d40b5a336f77cb1eecc
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-09-26 11:26:42 -0300

    feat: Apenas convite: MCPeerID.

--- project25/project25/ViewController.swift
+++ project25/project25/ViewController.swift
@@ -47,6 +47,23 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         
         images.insert(image, at: 0)
         collectionView.reloadData()
+        // 1
+        guard let mcSession = mcSession else { return }
+        // 2
+        if mcSession.connectedPeers.count > 0 {
+            // 3
+            if let imageData = image.pngData() {
+                // 4
+                do {
+                    try mcSession.send(imageData, toPeers: mcSession.connectedPeers, with: .reliable)
+                } catch {
+                    // 5
+                    let ac = UIAlertController(title: "Send error", message: error.localizedDescription, preferredStyle: .alert)
+                    ac.addAction(UIAlertAction(title: "OK", style: .default))
+                    present(ac, animated: true)
+                }
+            }
+        }
     }
     
     func startHosting(action: UIAlertAction) {
@@ -60,6 +77,48 @@ class ViewController: UICollectionViewController, UINavigationControllerDelegate
         let mcBrowser = MCBrowserViewController(serviceType: "hws-project25", session: mcSession)
         mcBrowser.delegate = self
         present(mcBrowser, animated: true)
+    }
+    // MARK: - Session
+    func session(_ session: MCSession, didReceive stream: InputStream, withName streamName: String, fromPeer peerID: MCPeerID) {
+
+    }
+
+    func session(_ session: MCSession, didStartReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, with progress: Progress) {
+
+    }
+
+    func session(_ session: MCSession, didFinishReceivingResourceWithName resourceName: String, fromPeer peerID: MCPeerID, at localURL: URL?, withError error: Error?) {
+
+    }
+    // MARK: - Browser
+    func browserViewControllerDidFinish(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    func browserViewControllerWasCancelled(_ browserViewController: MCBrowserViewController) {
+        dismiss(animated: true)
+    }
+    
+    func session(_ session: MCSession, peer peerID: MCPeerID, didChange state: MCSessionState) {
+        switch state {
+        case .connected:
+            print("Conectado: \(peerID.displayName)")
+        case .connecting:
+            print("Conectando: \(peerID.displayName)")
+        case .notConnected:
+            print("Não Conectado: \(peerID.displayName)")
+        @unknown default:
+            print("Estado desconhecido: \(peerID.displayName)")
+        }
+    }
+    
+    func session(_ session: MCSession, didReceive data: Data, fromPeer peerID: MCPeerID) {
+        DispatchQueue.main.async { [weak self] in
+            if let image = UIImage(data: data) {
+                self?.images.insert(image, at: 0)
+                self?.collectionView.reloadData()
+            }
+        }
     }
     
     @objc func importPicture() {
