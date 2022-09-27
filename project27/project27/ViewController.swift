e1aca98070aef440b9d85739cdcff29ffc9aab81
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-09-27 15:37:56 -0300

    feat: Ellipses e checkerboards.

--- project27/project27/ViewController.swift
+++ project27/project27/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
         let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
         
         let img = renderer.image { ctx in
-            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 20, dy: 20)
             
             ctx.cgContext.setFillColor(UIColor.purple.cgColor)
             ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
@@ -25,6 +25,42 @@ class ViewController: UIViewController {
             
             ctx.cgContext.addRect(rectangle)
             ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = img
+    }
+    
+    func drawCircle() {
+        
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512).insetBy(dx: 20, dy: 20)
+            
+            ctx.cgContext.setFillColor(UIColor.purple.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            
+            ctx.cgContext.addEllipse(in: rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = img
+    }
+    
+    func drawCheckerboard() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let img = renderer.image { ctx in
+            ctx.cgContext.setFillColor(UIColor.black.cgColor)
+            
+            for row in 0 ..< 8 {
+                for col in 0 ..< 8 {
+                    if (row + col).isMultiple(of: 2) {
+                        ctx.cgContext.fill(CGRect(x: col * 64, y: row * 64, width: 64, height: 64))
+                    }
+                }
+            }
         }
         
         imageView.image = img
@@ -46,6 +82,12 @@ class ViewController: UIViewController {
         switch currentDrawType {
         case 0:
             drawRectangle()
+            
+        case 1:
+            drawCircle()
+            
+        case 2:
+            drawCheckerboard()
             
         default:
             break
