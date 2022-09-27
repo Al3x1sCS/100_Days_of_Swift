a664b8ed27771bc54b30f81fe94bd45bd5e9528b
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-09-27 16:16:28 -0300

    feat: Iniciando projeto 28.

---
+++ project28/project28/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project28
+//
+//  Created by user on 27/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
