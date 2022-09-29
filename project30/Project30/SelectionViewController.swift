e8bd13d93bf4b464a8b852b22c4b68a8e640b295
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-09-29 16:15:40 -0300

    fix: Corrigindo os bugs - sombras lentas.

--- project30/Project30/SelectionViewController.swift
+++ project30/Project30/SelectionViewController.swift
@@ -62,24 +62,26 @@ class SelectionViewController: UITableViewController {
 		let currentImage = items[indexPath.row % items.count]
 		let imageRootName = currentImage.replacingOccurrences(of: "Large", with: "Thumb")
 		let path = Bundle.main.path(forResource: imageRootName, ofType: nil)!
-		let original = UIImage(contentsOfFile: path)!
-
-		let renderer = UIGraphicsImageRenderer(size: original.size)
-
-		let rounded = renderer.image { ctx in
-			ctx.cgContext.addEllipse(in: CGRect(origin: CGPoint.zero, size: original.size))
-			ctx.cgContext.clip()
-
-			original.draw(at: CGPoint.zero)
-		}
+        let original = UIImage(contentsOfFile: path)!
+        
+        let renderRect = CGRect(origin: .zero, size: CGSize(width: 90, height: 90))
+        let renderer = UIGraphicsImageRenderer(size: renderRect.size)
+        
+        let rounded = renderer.image { ctx in
+            ctx.cgContext.addEllipse(in: renderRect)
+            ctx.cgContext.clip()
+            
+            original.draw(in: renderRect)
+        }
 
 		cell.imageView?.image = rounded
 
 		// give the images a nice shadow to make them look a bit more dramatic
 		cell.imageView?.layer.shadowColor = UIColor.black.cgColor
 		cell.imageView?.layer.shadowOpacity = 1
-		cell.imageView?.layer.shadowRadius = 5
+		cell.imageView?.layer.shadowRadius = 10
 		cell.imageView?.layer.shadowOffset = CGSize.zero
+        cell.imageView?.layer.shadowPath = UIBezierPath(ovalIn: renderRect).cgPath
 
 		// each image stores how often it's been tapped
 		let defaults = UserDefaults.standard
