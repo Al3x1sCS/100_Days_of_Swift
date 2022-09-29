8fab914e70fe14031a9da0a11b3a12242e2e48b7
Al3x1sCS <prog.alexis.cs@protonmail.com>
2022-09-29 16:59:57 -0300

    fix: Corrigindo os bugs - Ficando sem memória.

--- project30/Project30/SelectionViewController.swift
+++ project30/Project30/SelectionViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class SelectionViewController: UITableViewController {
 	var items = [String]() // this is the array that will store the filenames to load
-	var viewControllers = [UIViewController]() // create a cache of the detail view controllers for faster loading
+//	var viewControllers = [UIViewController]() // create a cache of the detail view controllers for faster loading
 	var dirty = false
 
     override func viewDidLoad() {
@@ -102,8 +102,8 @@ class SelectionViewController: UITableViewController {
 		// mark us as not needing a counter reload when we return
 		dirty = false
 
-		// add to our view controller cache and show
-		viewControllers.append(vc)
+//		// add to our view controller cache and show
+//		viewControllers.append(vc)
 		navigationController!.pushViewController(vc, animated: true)
 	}
 }
